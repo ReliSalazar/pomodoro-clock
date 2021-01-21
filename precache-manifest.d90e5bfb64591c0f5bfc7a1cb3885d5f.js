34976865aaf8824409835c24d0e1508233957954
relisalazar <naitvenefy@gmail.com>
2021-01-21 04:06:21 -0600

    Updates

---
+++ precache-manifest.d90e5bfb64591c0f5bfc7a1cb3885d5f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4880788297606b3e561dc2cd89b41db",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "3581e0220cbe880db50c",
+    "url": "/pomodoro-clock/static/css/main.106dbd42.chunk.css"
+  },
+  {
+    "revision": "c9cf4c27b7211e12371b",
+    "url": "/pomodoro-clock/static/js/2.6130551b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pomodoro-clock/static/js/2.6130551b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3581e0220cbe880db50c",
+    "url": "/pomodoro-clock/static/js/main.32197461.chunk.js"
+  },
+  {
+    "revision": "d420e891230cef74bfe7",
+    "url": "/pomodoro-clock/static/js/runtime-main.cd73fa2f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/pomodoro-clock/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
